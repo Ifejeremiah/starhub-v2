14180fd8ab3f6bf704a5c28a0750d28b9ce7af26
Ife Jeremiah <jerryroboy.16@gmail.com>
2021-11-30 11:45:53 +0100

    Making site responsive, added back-to-top button, modified js file.

--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -1,10 +1,39 @@
 $(function () {
+  const $window = $(window);
+  const $menu = $('.con-menu');
+  const $navbar = $('.con-navbar');
+  const $btn = $('.con-top-btn');
+
   // Year Stamp
   $('.copyrights .yearstamp').text(new Date().getFullYear());
 
   // Navbar Menu
   $('.navbar .con-toggle').on('click', function () {
     $('.toggle').toggleClass('active');
-    $('.con-menu').toggleClass('active');
+    $menu.toggleClass('active');
   });
+
+  // Animation For Navigation Bar
+  $window.on('scroll', function () {
+    if ($window.scrollTop() >= 50) {
+      $navbar.addClass('active');
+    } else {
+      $navbar.removeClass('active');
+    }
+  });
+
+  // Animation For Back To Top Button On Scroll
+  $window.on('scroll', function () {
+    if ($window.scrollTop() >= 200) {
+      $btn.fadeIn(100);
+    } else {
+      $btn.fadeOut(400);
+    }
+  });
+
+  // Animation For Back To Top Button
+  $btn.on('click', function () {
+    $window.scrollTop(0);
+  });
+
 });
